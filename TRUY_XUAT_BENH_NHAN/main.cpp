e5c3312d7273a6c1fa2aab69e28ae804375ce9a3
nghoanghuy12042007-dot <nghoanghuy12042007@gmail.com>
2026-09-22 10:29:00 +0700

    Update main.cpp

---
+++ TRUY_XUAT_BENH_NHAN/main.cpp
@@ -0,0 +1,16 @@
+#include "ClinicQueueManager.h"
+#include <crow_all.h>
+
+int main() {
+    crow::SimpleApp app;
+    ClinicQueueManager queue; // Khởi tạo Cấu trúc dữ liệu
+
+    // API Endpoint: /api/call-next
+    CROW_ROUTE(app, "/api/call-next")
+    ([&queue](){
+        return queue.callNextPatientWeb(); // Gọi hàm xử lý và trả về JSON
+    });
+
+    app.port(8080).multithreaded().run();
+    return 0;
+}
